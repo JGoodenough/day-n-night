b226fb09c31af11f74c612c71807b2704dd51f95
Jesse Goodenough <jesse.goodenough@gmail.com>
2023-05-18 21:51:43 -0400

    create mainlayout component

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,20 @@
+import { FC, ReactNode } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+type MainLayoutProps = {
+  children?: ReactNode;
+};
+
+const MainLayout: FC<MainLayoutProps> = ({ children }) => {
+  return <View style={styles.MainLayout}>{children}</View>;
+};
+
+export default MainLayout;
+
+const styles = StyleSheet.create({
+  MainLayout: {
+    flex: 1,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+});
--- components/screens/HomeScreen.tsx
+++ components/screens/HomeScreen.tsx
@@ -6,26 +6,31 @@ import SunriseSunset from '../SunriseSunset';
 import { AppFontSizes } from '../../constants/ui';
 import { useContext } from 'react';
 import { LocationContext } from '../../context/location';
+import MainLayout from '../layouts/MainLayout';
 
 const HomeScreen = () => {
   const { location, setLocation, locationAddresses, locationErrorMessage } =
     useContext(LocationContext);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <AddressLookup location={location} setLocation={setLocation} />
-      <CurrentLocation
-        latitude={location?.coords?.latitude}
-        longitude={location?.coords?.longitude}
-        locationAddress={locationAddresses?.[0]}
-        errorMessage={locationErrorMessage}
-      />
-      <StatusBar style="auto" />
-      <SunriseSunset
-        lat={location?.coords?.latitude ? `${location.coords.latitude}` : ''}
-        lng={location?.coords?.longitude ? `${location.coords.longitude}` : ''}
-      />
-    </SafeAreaView>
+    <MainLayout>
+      <SafeAreaView style={styles.container}>
+        <AddressLookup location={location} setLocation={setLocation} />
+        <CurrentLocation
+          latitude={location?.coords?.latitude}
+          longitude={location?.coords?.longitude}
+          locationAddress={locationAddresses?.[0]}
+          errorMessage={locationErrorMessage}
+        />
+        <StatusBar style="auto" />
+        <SunriseSunset
+          lat={location?.coords?.latitude ? `${location.coords.latitude}` : ''}
+          lng={
+            location?.coords?.longitude ? `${location.coords.longitude}` : ''
+          }
+        />
+      </SafeAreaView>
+    </MainLayout>
   );
 };
 
@@ -33,13 +38,9 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
     justifyContent: 'space-between',
     alignContent: 'center',
     alignItems: 'center',
-    padding: 10,
-    paddingTop: 0,
-    paddingBottom: 0,
     fontSize: AppFontSizes.BodyFontSize,
   },
 });
