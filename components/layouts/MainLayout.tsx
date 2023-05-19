0e450e30313001838d2ff52a57ee34a2c45d8eda
Jesse Goodenough <jesse.goodenough@gmail.com>
2023-05-19 10:25:44 -0400

    Add acknowledgements back in

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -1,12 +1,18 @@
 import { FC, ReactNode } from 'react';
 import { View, StyleSheet } from 'react-native';
+import Acknowledgements from '../Acknowledgements';
 
 type MainLayoutProps = {
   children?: ReactNode;
 };
 
 const MainLayout: FC<MainLayoutProps> = ({ children }) => {
-  return <View style={styles.MainLayout}>{children}</View>;
+  return (
+    <View style={styles.MainLayout}>
+      {children}
+      <Acknowledgements />
+    </View>
+  );
 };
 
 export default MainLayout;
