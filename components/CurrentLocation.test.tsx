3e5b891d9eaae437e557dcd1d830fb74477ee433
Jesse Goodenough <jesse.goodenough@gmail.com>
2023-06-01 16:49:41 -0400

    WIP CurrentLocaiton component unit tests

--- components/CurrentLocation.test.tsx
+++ components/CurrentLocation.test.tsx
@@ -1,7 +1,14 @@
+import renderer from 'react-test-renderer';
+
 import CurrentLocation from './CurrentLocation';
 
 describe('<CurrentLocation />', () => {
   it('should be defined', () => {
     expect(CurrentLocation).toBeDefined();
+  });
+
+  it('should render nothing if NO location address is passed', () => {
+    const tree = renderer.create(<CurrentLocation />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
